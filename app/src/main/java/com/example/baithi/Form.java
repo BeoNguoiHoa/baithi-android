298bb7c61fb98086b69c3fbbdfccff30a5fbab8b
quanthanhtuc@gmail.com <quanthanhtuc@gmail.com>
2022-04-08 19:48:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baithi/Form.java
@@ -0,0 +1,15 @@
+package com.example.baithi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
